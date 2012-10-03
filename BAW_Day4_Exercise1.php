b43ec637c5355f17e2b7066b7077b700ce8dd2b6
Joanne Icken <ickenj@gmail.com>
2012-10-02 20:43:27 -0400

    committing exercise1

---
+++ BAW_Day4_Exercise1.php
@@ -0,0 +1,11 @@
+<?php
+
+$myname = "Joanne Icken";
+$bdate = "May 20, 1959";
+$fcolor = "purple";
+
+print "My name is " . $myname . "\n";
+print "I was born " . $bdate . "\n";
+print "My favorite color is " . $fcolor . "\n";
+
+?>
