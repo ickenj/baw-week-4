ebd47a47db2953f06f6f72f444bc955eefc4ffa4
Joanne Icken <ickenj@gmail.com>
2012-11-12 13:12:27 -0500

    new commit

---
+++ BAW_Day4_Exercise2.php
@@ -0,0 +1,17 @@
+<?php
+
+/* How many minutes are there in a week in the Bilky Way, a near parallel gallaxy if there are 26 hours in a day. */
+
+$DaysPerWeek = 7;
+$HoursPerDay = 24;
+$MinutesPerHour = 60;
+
+$MinutesPerWeek = ($HoursPerDay * $MinutesPerHour) * $DaysPerWeek;
+print "Minutes Per Week on earth are " . $MinutesPerWeek . "\n";
+
+$HoursPerDay = 26;
+
+$MinutesPerWeek = ($HoursPerDay * $MinutesPerHour) * $DaysPerWeek;
+print "Minutes Per Week in the Bilky Way are " . $MinutesPerWeek . "\n";
+
+?>
